33704b9396a94b2b05036d09140861b6b3ef660c
lix <lixiang852000@163.com>
2020-06-15 15:24:19 +0800

    初始化

--- src/main/java/com/lxcm/sys/service/impl/RoleServiceImpl.java
+++ src/main/java/com/lxcm/sys/service/impl/RoleServiceImpl.java
@@ -1,10 +1,15 @@
 package com.lxcm.sys.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lxcm.sys.dao.RoleDao;
 import com.lxcm.sys.entity.RoleEntity;
 import com.lxcm.sys.service.IRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.Serializable;
 
 /**
  * @Classname RoleServiceImpl
@@ -14,6 +19,5 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class RoleServiceImpl extends ServiceImpl<RoleDao, RoleEntity> implements IRoleService {
-
 
 }
